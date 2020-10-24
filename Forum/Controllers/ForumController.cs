3b6fb0257844a9d24cd6d7ec5d79488fec25e2ee
Kirill-Spesivtsev <ksby8819@gmail.com>
2020-10-24 23:05:57 +0300

    Add service layer

---
+++ Forum/Controllers/ForumController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumProject.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum forumService;
+
+        public ForumController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Forum/Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using ForumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumProject.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int Id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
