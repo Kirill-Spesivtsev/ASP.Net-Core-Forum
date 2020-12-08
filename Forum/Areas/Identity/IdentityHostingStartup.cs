931188ac8a8ee27bcadfbc38eab1e64e9c926533
Kirill-Spesivtsev <ksby8819@gmail.com>
2020-12-08 15:50:34 +0300

    Generated code for some Identity models and views. Changed register form

---
+++ Forum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ForumProject.Data;
+using ForumProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ForumProject.Areas.Identity.IdentityHostingStartup))]
+namespace ForumProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
