f01e98a2e6752b029572b7bb110f5c0aabdbf223
Kirill-Spesivtsev <ksby8819@gmail.com>
2021-01-20 21:27:53 +0300

    Created ProfileController, ApplicationUserService, UploadService

---
+++ Forum/Interfaces/IApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using ForumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumProject.Interfaces
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+        Task SetUserProfileImage(string id, Uri uri);
+        Task IncrementRating(string id, Type type);
+    }
+}
--- Forum/Services/ApplicationUserService.cs
+++ Forum/Services/ApplicationUserService.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using ForumProject.Data;
+using ForumProject.Interfaces;
+using ForumProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ForumProject.Services
 {
-    public class ApplicationUserService
+    public class ApplicationUserService : IApplicationUser
     {
+        public readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll().FirstOrDefault(user => user.Id == id);
+        }
+
+        public Task IncrementRating(string id, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetUserProfileImage(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
     }
 }
